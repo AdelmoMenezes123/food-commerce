0ab5a7925d449fd28e95217e495ba831a8b497ee
Adelmo Menezes <adelmo@connectlab.com.br>
2024-04-27 18:11:07 -0300

    feat: chama os dados na listagem de pizzas

--- src/pages/Main/Pizzas/index.tsx
+++ src/pages/Main/Pizzas/index.tsx
@@ -1,32 +1,24 @@
+import { useEffect, useState } from 'react'
 import { Head } from '../../../components/Head'
 import SnackTitle from '../../../components/SnackTitle'
 import { Snacks } from '../../../components/Snacks'
+import { getPizzas } from '../../../services/api'
 
 export default function Pizzas() {
-  const data = [
-    {
-      id: 1,
-      snack: 'pizza',
-      name: 'Calabresa',
-      description: 'Pizza recheada coom calabresa e cebola tendo uma borda recheada com catupiry',
-      price: 25.5,
-      image: 'https://imgur.com/5rjJGkV.png',
-    },
-    {
-      id: 2,
-      snack: 'pizza',
-      name: 'Portuguesa',
-      description:
-        'pizza recheada com presunto, mussarela, ovo, cebola e azeitona tendendo uma borda recheada com catupiry',
-      price: 28.59,
-      image: 'https://imgur.com/WCoyGoI.jpg',
-    },
-  ]
+  const [pizzas, setPizzas] = useState([])
+
+  useEffect(() => {
+    ;(async () => {
+      const pizzaRequest = await getPizzas()
+
+      setPizzas(pizzaRequest.data)
+    })()
+  })
   return (
     <>
       <Head title='Pizzas' description='Nossas melhores pizzas' />
       <SnackTitle>Pizzas</SnackTitle>
-      <Snacks snacks={data}></Snacks>
+      <Snacks snacks={pizzas}></Snacks>
     </>
   )
 }
