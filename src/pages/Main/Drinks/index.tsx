c5daed7f82207eb238def0ace61ddff9b209a5cc
Adelmo Menezes <adelmo@connectlab.com.br>
2024-04-27 18:07:19 -0300

    feat: chama os dados na listagem de drinks

--- src/pages/Main/Drinks/index.tsx
+++ src/pages/Main/Drinks/index.tsx
@@ -1,39 +1,25 @@
+import { useEffect, useState } from 'react'
 import { Head } from '../../../components/Head'
 import SnackTitle from '../../../components/SnackTitle'
 import { Snacks } from '../../../components/Snacks'
+import { getDrinks } from '../../../services/api'
 
 export default function Drinks() {
-  const data = [
-    {
-      id: 1,
-      snack: 'drink',
-      name: 'Coca-Cola 2L',
-      description: 'Atradicional coca-cola que a familia adora',
-      price: 8.5,
-      image: 'https://i.imgur.com/Lg3aKhf.jpg',
-    },
-    {
-      id: 2,
-      snack: 'drink',
-      name: 'Guaraná Antartica',
-      description: 'O irresistível guaraná antartica',
-      price: 6.5,
-      image: 'https://i.imgur.com/hOBrOIm.jpg',
-    },
-    {
-      id: 3,
-      snack: 'drink',
-      name: 'Suco de abacaxi',
-      description: 'Um delicioso suco de abacaxi',
-      price: 8,
-      image: 'https://i.imgur.com/VV9qTMh.jpg',
-    },
-  ]
+  const [drinks, setDrinks] = useState([])
+
+  useEffect(() => {
+    ;(async () => {
+      const drinkRequest = await getDrinks()
+
+      setDrinks(drinkRequest.data)
+    })()
+  })
+
   return (
     <>
       <Head title='Drinks' description='Nossos melhores drinks' />
       <SnackTitle>Drinks</SnackTitle>
-      <Snacks snacks={data}></Snacks>
+      <Snacks snacks={drinks}></Snacks>
     </>
   )
 }
