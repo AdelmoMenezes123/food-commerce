9a7c40b7b238b630352a8dc54280e0a508a8a791
Adelmo Menezes <adelmo@connectlab.com.br>
2024-07-06 16:33:57 -0300

    feat: definindo a estrutura de tipos do projeto

--- src/pages/Main/Burgers/index.tsx
+++ src/pages/Main/Burgers/index.tsx
@@ -2,10 +2,11 @@ import { useEffect, useState } from 'react'
 import { Head } from '../../../components/Head'
 import SnackTitle from '../../../components/SnackTitle'
 import { Snacks } from '../../../components/Snacks'
+import { SnackData } from '../../../interfaces/SnackData'
 import { getBurgers } from '../../../services/api'
 
 export default function Burgers() {
-  const [burgers, setBurgers] = useState([])
+  const [burgers, setBurgers] = useState<SnackData[]>([])
 
   useEffect(() => {
     ;(async () => {
--- src/pages/Main/Drinks/index.tsx
+++ src/pages/Main/Drinks/index.tsx
@@ -2,10 +2,11 @@ import { useEffect, useState } from 'react'
 import { Head } from '../../../components/Head'
 import SnackTitle from '../../../components/SnackTitle'
 import { Snacks } from '../../../components/Snacks'
+import { SnackData } from '../../../interfaces/SnackData'
 import { getDrinks } from '../../../services/api'
 
 export default function Drinks() {
-  const [drinks, setDrinks] = useState([])
+  const [drinks, setDrinks] = useState<SnackData[]>([])
 
   useEffect(() => {
     ;(async () => {
